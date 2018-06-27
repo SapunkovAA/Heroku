c65824789a3a46e8f07b23996771466ca1c6929e
SapunkovAA <AASapunkovv@gmail.com>
2018-06-27 10:16:07 +0300

    fix8

--- api/routes/loginByGoogle.js
+++ api/routes/loginByGoogle.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy; //не правильный рекваер,правильный инсталл авторизации, 2е не подключен класс
+//3е 
 // const User = require('../models/users');
 const Social = require('../models/socilalUsers');
 const PORT = require('../../config/port');
@@ -14,26 +15,26 @@ const userGoogle = () => {
           callbackURL: `http://infinite-ridge-68592.herokuapp.com/users/login/google/callback`
      }, (req, accessToken, refreshToken, profile, done) => {
           console.log('qwe', profile);
-          let data = profile._json; //статический объект, это свойство которое хранит 
-          Social.findOne({
-                    id: data.id
-               })
-               .exec()
-               .then(user => {
-                    if (!user) {
-                         const newUser = new Social({
-                              id: data.id,
-                              displayName: data.displayName,
-                              provider: data.provider
-                         });
-                         newUser.save();
-                         done(null, newUser);
-                    }
+          let data = profile._json;
 
-               })
-               .catch(err => {
-                    res.status(500).json(err)
-               });
+          Social.findOne({id: data.id})
+          .exec()
+          .then(user => {
+              if (!user) {
+                  const newUser = new Social({
+                      id: data.id,
+                      displayName: data.displayName,
+                      provider: data.provider
+                  });
+                  newUser.save();
+                  done(null, newUser);
+              }
+
+          })
+          .catch(err => {
+              console.log(err);
+          })
+
      }));
 };
 module.exports = userGoogle;
--- api/routes/users.js
+++ api/routes/users.js
@@ -16,110 +16,112 @@ userFacebook();
 userGoogle();
 
 router.post('/signup', function (req, res, next) {
-     User.findOne({
-               email: req.body.email,
-          })
-          .exec()
-          .then(user => {
-               if (user) {
-                    res.status(200).json({
-                         message: "there is a user already with this email"
-                    })
-               } else {
-                    if (req.body.password) {
-                         bcrypt.hash(req.body.password, 10)
-                              .then(hash => {
-                                   const user = new User({
-                                        _id: new mongoose.Types.ObjectId(),
-                                        email: req.body.email,
-                                        password: hash
-                                   });
-                                   return user.save();
-                              })
-                              .then(() => {
-                                   res.status(201).json({
-                                        message: "User was created",
-                                   });
-                              })
-                              .catch(err => {
-                                   res.status(500).json(err)
-                              });
-                    } else {
-                         res.status(400).json({
-                              error: "No password field"
-                         });
-                    }
+  User.findOne({
+      email: req.body.email,
+    })
+    .exec()
+    .then(user => {
+      if (user) {
+        res.status(200).json({
+          message: "there is a user already with this email"
+        })
+      } else {
+        if (req.body.password) {
+          bcrypt.hash(req.body.password, 10)
+            .then(hash => {
+              const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash
+              });
+              return user.save();
+            })
+            .then(() => {
+              res.status(201).json({
+                message: "User was created",
+              });
+            })
+            .catch(err => {
+              res.status(500).json(err)
+            });
+        } else {
+          res.status(400).json({
+            error: "No password field"
+          });
+        }
 
-               }
-          })
+      }
+    })
 
 });
 
 
 router.post('/login', function (req, res, next) {
-     User.findOne({
-               email: req.body.email,
-          })
-          .exec()
-          .then(user => {
-               if (user) {
-                    bcrypt.compare(req.body.password, user.password, function (err, result) {
-                         if (err) {
-                              res.status(500).json({
-                                   message: "password  is not match"
-                              })
-                         } else {
-                              const token = jwt.sign({
-                                   email: user.email,
-                                   _id: user._id
-                              }, 'secretWord');
-                              res.status(200).json({
-                                   message: "Congratulation",
-                                   userToken: token
-                              })
+  User.findOne({
+      email: req.body.email,
+    })
+    .exec()
+    .then(user => {
+      if (user) {
+        bcrypt.compare(req.body.password, user.password, function (err, result) {
+          if (err) {
+            res.status(500).json({
+              message: "password  is not match"
+            })
+          } else {
+            const token = jwt.sign({
+              email: user.email,
+              _id: user._id
+            }, 'secretWord');
+            res.status(200).json({
+              message: "Congratulation",
+              userToken: token
+            })
 
-                         }
-                    })
+          }
+        })
 
-               } else {
-                    res.status(404).json({
-                         message: "email not found"
-                    })
-               }
-          })
+      } else {
+        res.status(404).json({
+          message: "email not found"
+        })
+      }
+    })
 
 })
 router.get('/', function (req, res, next) {
-     User.find()
-          .select('-password')
-          .exec()
-          .then(users => {
-               res.status(200).json({
-                    message: "list of all users",
-                    allUsers: users
-               });
-          })
-          .catch(err => {
-               res.status(500).json(err)
-          });
+  User.find()
+    .select('-password')
+    .exec()
+    .then(users => {
+      res.status(200).json({
+        message: "list of all users",
+        allUsers: users
+      });
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    });
 
 });
-
-
-
 
 router.get('/login/facebook', passport.authenticate('facebook'));
 router.get('/login/facebook/callback', passport.authenticate('facebook'), (req, res) => {
-     res.send('You are wellcome to facebook')
+  res.send('You are wellcome to facebook')
 });
 
 router.get('/login/google', passport.authenticate('google', {
-     scope: ['profile']
+  scope: ['profile'] //???????{ scope: ['https://www.googleapis.com/auth/plus.login'] }; что такое скоуп
 }));
 
-
 router.get('/login/google/callback', (req, res) => {
-     res.redirect(`http://infinite-ridge-68592.herokuapp.com/users/login/google/callback`)
+  res.redirect(`./../routes/products.js`)
 });
+//здесь ап вместо роут
+// router.get('/login/google/callback', 
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     res.redirect('/');
+//   });
 
 module.exports = router;
